8e7c0931a61097a1d5d00cc373937dff419c719d
Chelbyn <sebastianvelasquez456@gmail.com>
2023-09-28 23:30:10 -0500

    Corrige errores de los comentarios

--- plex.py
+++ plex.py
@@ -4,27 +4,31 @@ plex.py
 Analizador Lexico para el lenguaje pascal
 '''
 import sly
+from prettytable import PrettyTable
 
 class Lexer(sly.Lexer):
     tokens = {
         # Palabras Reservadas
         FUN, BEGIN, END, SKIP, BREAK, WHILE, DO,
-        IF, THEN, PRINT, WRITE, READ, RETURN, 
-        
+        IF, THEN, PRINT, WRITE, READ, RETURN,
+
         # Operadores de Relacion
         LT, LE, GT, GE, ET, DF,
-        
-        #Operadores logicos
-        AND, OR, NOT,
-        
-        # Literales
-        INTEGER, FLOAT, STRING, NEWLINE, ASSIGNOP,
-        
-        # Datatype
-        INTTYPE, FLOATTYPE,
 
-        # Otros Simbolos
+        # Operadores logicos
+        AND, OR, NOT,
+
+        # Literales
+        ICONST, FCONST, STRING, ASSIGNOP,
+
+        # Datatype
+        INT, FLOAT,
+
+        # Identificador
         ID,
+
+        # Comentarios
+        COMMENT
     }
     literals = '+-*/=,;():[]"'
 
@@ -34,73 +38,93 @@ class Lexer(sly.Lexer):
     # expresiones regulares
     STRING = r'".*"'
 
+    keywords = {'and', 'begin', 'break', 'do', 'else', 'end', 'float', 'fun', 'if', 'int', 'not', 'or', 'print',
+                'read', 'return', 'skip', 'then', 'while', 'write'}
+
     @_(r'[a-zA-Z_]+(\w|_)*')
     def ID(self, t):
-        if t.value.upper() in self.tokens and t.value.upper() != 'ID':
+        if t.value in self.keywords:
             t.type = t.value.upper()
         return t
 
-    FUN    = r'fun'
-    BEGIN  = r'begin'
-    END    = r'end'
-    SKIP   = r'skip'
-    BREAK  = r'break'
-    WHILE  = r'while'
-    DO     = r'do'
-    IF     = r'if'
-    THEN   = r'then'
-    PRINT  = r'print'
-    WRITE  = r'write'
-    READ   = r'read'
+    FUN = r'fun'
+    BEGIN = r'begin'
+    END = r'end'
+    SKIP = r'skip'
+    BREAK = r'break'
+    WHILE = r'while'
+    DO = r'do'
+    IF = r'if'
+    THEN = r'then'
+    PRINT = r'print'
+    WRITE = r'write'
+    READ = r'read'
     RETURN = r'return'
 
-    #operadores de relacion
-    LE= r'<='
-    LT= r'<'
-    GE= r'>='
-    GT= r'>'
-    ET= r'=='
-    DF= r'!='
-    
-    #operadores logicos
+    # operadores de relacion
+    LE = r'<='
+    LT = r'<'
+    GE = r'>='
+    GT = r'>'
+    ET = r'=='
+    DF = r'!='
+
+    # operadores logicos
     AND = r'and'
     OR = r'or'
     NOT = r'not'
 
     ASSIGNOP = r':='
 
-    INTTYPE = r'int'
-    FLOATTYPE = r'float'
+    INT = r'int'
+    FLOAT = r'float'
+
+    @_(r'\/\*([^*]|(\*+[^*/]))*\*+\/')
+    def COMMENT(self, t):
+        self.lineno += t.value.count('\n')
+        return t
 
     @_(r'\n+')
-    def NEWLINE(self, t):
-        self.lineno += t.value.count('\n')
-    
-    @_(r'(\d+\.\d*|\d*\.\d+)(E-?\d+)?|[1-9]\d*E-?\d+')
-    def FLOAT(self, t):
-        t.value = float(t.value)
+    def ignore_newline(self, t):
+        self.lineno += len(t.value)
+
+    @_(r'(\d+\.\d+)(e-?\d+)?|[0-9]\d*e-?\d+')
+    def FCONST(self, t):
         return t
 
     @_(r'\d+')
-    def INTEGER(self, t):
-        t.value = int(t.value)
-        return t
+    def ICONST(self, t):
+        if len(t.value) > 1 and t.value[0] == '0':
+            self.error(t, error_type=1)
+        else:
+            t.value = int(t.value)
+            return t
 
-    def error(self, t):
-        print(f"Illegal character '{t.value[0]}'")
-        self.index += 1
+    def error(self, t, error_type=0):
+        if error_type == 0:
+            print(f'\033[91mERROR: Illegal character "{t.value[0]}" in line: {t.lineno}\033[0m')
+            self.index += 1
+        elif error_type == 1:
+            print(f'\033[91mERROR: Leading zeros not supported in integer {t.value}, line: {t.lineno}\033[0m')
+
 
 def main(argv):
-    '''if len(argv) != 2:
+    if len(argv) != 2:
         print(f"Usage: python {argv[0]} filename")
-        exit(1)'''
-    
+        exit(1)
+
     lex = Lexer()
-    #txt = open(argv[1]).read()
-    txt = open('test2.pl0').read()
+    txt = open('test1/' + argv[1]).read()
+    #txt = open('test1/badcomment.pl0').read()
+
+    tokens_table = PrettyTable()
+    tokens_table.align = 'l'
+    tokens_table.field_names = ['TOKEN', 'LEXEMA', 'LINENO']
 
     for tok in lex.tokenize(txt):
-        print(tok)
+        tokens_table.add_row([tok.type, tok.value, tok.lineno])
+
+    print(tokens_table)
 
 
 if __name__ == '__main__':
