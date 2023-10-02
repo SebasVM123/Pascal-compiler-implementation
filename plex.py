bf8d8319e5131afea8955f8ab291097b95f5da37
Sebitax <sebastianvelasquez456@gmail.com>
2023-10-01 20:09:24 -0500

    Agrega la union de cadenas seguidas y la deteccion de error de caracteres de escape

--- plex.py
+++ plex.py
@@ -4,6 +4,7 @@ plex.py
 Analizador Lexico para el lenguaje pascal
 '''
 import sly
+import re
 from prettytable import PrettyTable
 
 class Lexer(sly.Lexer):
@@ -40,7 +41,34 @@ class Lexer(sly.Lexer):
         self.lineno += len(t.value)
 
     # expresiones regulares
-    STRING = r'"[^"\n]*"'
+    @_(r'"([^"\n\\]*(\\.?[^"\n\\]*)*)"([ ]*("([^"\n\\]*(\\.?[^"\n\\]*)*)")*)*')
+    def STRING(self, t):
+        t.value = t.value.replace('\\"', '&escquote&')
+
+        splited_string = t.value.split('"')[1::2]
+
+        t.value = '"' + ''.join(splited_string) + '"'
+        t.value = t.value.replace('&escquote&', '\\"')
+
+        escape_characters = {'"', 'n', '\\'}
+
+        i = 0
+        unknown_escapechar_error = False
+        while i < len(t.value):
+            if t.value[i] == '\\':
+                if i + 1 < len(t.value) and t.value[i + 1] not in escape_characters:
+                    unknown_escapechar_error = True
+                    self.error(t, error_type=5, extra_info=t.value[i + 1])
+                i += 2
+            else:
+                i += 1
+
+        if not unknown_escapechar_error:
+            return t
+
+    @_(r'"([^"\n\\]*(\\.?[^"\n\\]*)*)')
+    def UNCLOSED_STRING(self, t):
+        self.error(t, error_type=4)
 
     keywords = {'and', 'begin', 'break', 'do', 'else', 'end', 'float', 'fun', 'if', 'int', 'not', 'or', 'print',
                 'read', 'return', 'skip', 'then', 'while', 'write'}
@@ -108,7 +136,7 @@ class Lexer(sly.Lexer):
             t.value = int(t.value)
             return t
 
-    def error(self, t, error_type=0):
+    def error(self, t, error_type=0, extra_info=None):
         if error_type == 0:
             print(f'\033[91mERROR: Illegal character "{t.value[0]}" in line: {t.lineno}\033[0m')
             self.index += 1
@@ -118,6 +146,10 @@ class Lexer(sly.Lexer):
             print(f'\033[91mERROR: Unclosed comment at line: {t.lineno}\033[0m')
         elif error_type == 3:
             print(f'\033[91mERROR: {t.value} is not a valid name, line {t.lineno}\033[0m')
+        elif error_type == 4:
+            print(f'\033[91mERROR: Unclosed string at line {t.lineno}\033[0m')
+        elif error_type == 5:
+            print(f'\033[91mERROR: {extra_info} is not a valid escape character in line {t.lineno}\033[0m')
 
 
 def main(argv):
