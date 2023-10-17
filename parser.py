199aba3349dea6447c7af44155eb0f01b415efb1
AliciaV2002 <113649609+AliciaV2002@users.noreply.github.com>
2023-10-17 11:58:23 -0500

    Update parser.py
    Co-Authored-By: Sebitax <42723025+SebasVM123@users.noreply.github.com>

--- parser.py
+++ parser.py
@@ -43,11 +43,91 @@ class Parser(sly.Parser):
     @_('FLOAT [ "[" ICONST "]" ]')
     def float(self, p):
         pass
-
+    
+    @_('{ stmtlist } ";" stmt')
+    def stmtlist(self, p):
+        pass
+    
+    @_('WHILE relation DO stmt',
+       'IF relation THEN stmt',
+       'location ASSIGNOP expr',
+       'PRINT "(" STRING ")"',
+       'WRITE "(" expr ")"',
+       'READ "(" location ")"',
+       'RETURN expr',
+       'ID "(" exprlist ")"',
+       'SKIP',
+       'BREAK',
+       'BEGIN stmtlist END')
+    def stmt(self, p):
+        pass
+    
+    @_('ID','ID "[" expr "]"')
+    def location(self, p):
+        pass
+    
+    @_('logic_or')
+    def relation(self, p):
+        pass
+    
+    @_('logic_or OR { logic_and }')
+    def logic_or(self, p):
+        pass
+    
+    @_('logic_and AND { logic_not }')
+    def logic_and(self, p):
+        pass
+    
+    @_('NOT logic_not', 'equality')
+    def logic_not(self, p):
+        pass
+    
+    @_('equality DF comparison',
+       'equality ET comparison',
+       'comparison')
+    def equality(self, p):
+        pass
+    
+    @_('comparison GT expr',
+       'comparison GE expr',
+       'comparison LT expr',
+       'comparison LE expr',
+       'expr')
+    def comparison(self, p):
+        pass
+    
+    @_('factor "+" expr',
+       'factor "-" expr',
+       'factor')
+    def expr(self, p):
+        pass
+    
+    @_('term "*" factor',
+       'term "/" factor',
+       'term')
+    def factor(self, p):
+        pass
+    
+    @_('{ exprlist } "," expr')
+    def exprlist(self, p):
+        pass
+    
+    @_('ID  "(" exprlist ")"',
+       'ID  "[" exprlist "]"',
+       'ID',
+       'INT',
+       'FLOAT',
+       '"(" relation ")"',
+       'INT "(" expr ")"',
+       'FLOAT "(" expr ")"',
+       '"-" term',
+       '"+" term')
+    def term(self, p):
+        pass
+    
 
 txt = 'fun hola(a:int, b:float)'
 
 lex = Lexer()
 parser = Parser()
 parser.parse(lex.tokenize(txt))
-
